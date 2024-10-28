b833abef68332c10d8d79b5814f5304e872a743d
Ahmed Abdelaziz <113646264+ahmed3ab3az@users.noreply.github.com>
2024-10-28 22:34:12 +0300

    Update RmCommand.java
    Implementation

--- CLI/src/main/java/org/os/RmCommand.java
+++ CLI/src/main/java/org/os/RmCommand.java
@@ -4,7 +4,23 @@ import java.io.File;
 
 public class RmCommand {
 
+    /**
+     * Executes the rm command to delete a specified file or directory.
+     * If the specified file is a directory, an error message is shown.
+     *
+     * @param fileName The name of the file to be deleted
+     */
     public static void execute(String fileName) {
+        File file = new File(fileName);
 
+        if (!file.exists()) {
+            System.out.println("rm: cannot remove '" + fileName + "': No such file or directory");
+        } else if (file.isDirectory()) {
+            System.out.println("rm: cannot remove '" + fileName + "': Is a directory");
+        } else if (file.delete()) {
+            System.out.println("File '" + fileName + "' deleted");
+        } else {
+            System.out.println("rm: failed to delete '" + fileName + "'");
+        }
     }
 }
