d2017337a724235e4030ea5e1551ec4d99a7863a
MENNA HANY <menna.hany99.11@gmail.com>
2024-10-29 22:51:49 +0300

    touch creation implementation

--- CLI/src/main/java/org/os/TouchCommand.java
+++ CLI/src/main/java/org/os/TouchCommand.java
@@ -7,5 +7,12 @@ public class TouchCommand {
 
     public static void execute(String fileName) {
 
+        File file = new File(fileName);
+        try {
+             file.createNewFile(); // Returns true if created, false if already exists
+        } catch (IOException e) {
+            System.out.println("Error creating file: " + e.getMessage());
+
+
     }
-}
+}}
---
+++ CLI/src/test/java/org/os/TouchCommandTest.java
@@ -0,0 +1,7 @@
+package org.os;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TouchCommandTest {
+
+}
