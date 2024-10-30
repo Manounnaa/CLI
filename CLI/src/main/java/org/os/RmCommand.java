34c8d1a5b227588a328bbd02e71c02ff06e16397
Ahmed Abdelaziz <113646264+ahmed3ab3az@users.noreply.github.com>
2024-10-30 21:46:29 +0300

    Update RmCommand.java

--- CLI/src/main/java/org/os/RmCommand.java
+++ CLI/src/main/java/org/os/RmCommand.java
@@ -5,22 +5,27 @@ import java.io.File;
 public class RmCommand {
 
     /**
-     * Executes the rm command to delete a specified file or directory.
-     * If the specified file is a directory, an error message is shown.
+     * Executes the rm command to delete one or more specified files.
+     * If a specified file is a directory, an error message is shown.
      *
-     * @param fileName The name of the file to be deleted
+     * @param fileNames The names of the files to be deleted
      */
-    public static void execute(String fileName) {
-        File file = new File(fileName);
+    public static void execute(String[] fileNames) {
+        for (String fileName : fileNames) {
+            if ("rm".equals(fileName)) {
+                continue;
+            }
+            File file = new File(fileName);
 
-        if (!file.exists()) {
-            System.out.println("rm: cannot remove '" + fileName + "': No such file or directory");
-        } else if (file.isDirectory()) {
-            System.out.println("rm: cannot remove '" + fileName + "': Is a directory");
-        } else if (file.delete()) {
-            System.out.println("File '" + fileName + "' deleted");
-        } else {
-            System.out.println("rm: failed to delete '" + fileName + "'");
+            if (!file.exists()) {
+                System.out.println("rm: cannot remove '" + fileName + "': No such file or directory");
+            } else if (file.isDirectory()) {
+                System.out.println("rm: cannot remove '" + fileName + "': Is a directory");
+            } else if (file.delete()) {
+                System.out.println("File '" + fileName + "' deleted");
+            } else {
+                System.out.println("rm: failed to delete '" + fileName + "'");
+            }
         }
     }
 }
