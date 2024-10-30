a939c4c4632ade54ed78812ff933621457b9c1d8
Fatma Atta <fatmamaali@gmail.com>
2024-10-30 11:18:04 +0300

    LS command test

---
+++ CLI/src/test/java/org/os/LSCommandTest.java
@@ -0,0 +1,31 @@
+package test.java.org.os;
+
+import main.java.org.os.LsCommand;
+import org.junit.jupiter.api.Test;
+
+public class LSCommandTest {
+    @Test
+    public void ls() {
+        LsCommand.execute(new String[]{"ls", "*"});
+    }
+
+    @Test
+    public void lsMagicCard() {
+        LsCommand.execute(new String[]{"ls", "*.xml"});
+    }
+
+    @Test
+    public void lsRelativePath() {
+        LsCommand.execute(new String[]{"ls", "../"});
+    }
+
+    @Test
+    public void lsRelativePath2() {
+        LsCommand.execute(new String[]{"ls", "../../"});
+    }
+
+    @Test
+    public void lsRelativePath3() {
+        LsCommand.execute(new String[]{"ls", "../"});
+    }
+}
