bf9bd75c1f9fba8810dbdc7b6bfc6f83d8536af5
Fatma Atta <fatmamaali@gmail.com>
2024-10-30 11:12:27 +0300

    ls command

--- CLI/src/main/java/org/os/LsCommand.java
+++ CLI/src/main/java/org/os/LsCommand.java
@@ -1,8 +1,47 @@
-package org.os;
+package main.java.org.os;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class LsCommand {
 
     public static void execute(String[] tokens) {
+        if(tokens.length == 1){
+            lsDirectory(".", "*");
+        }
+        else{
+            // If tokens[1] has a wildcard, pass it as the pattern
+            String pattern = tokens[1].contains("*") ? tokens[1] : "*";
+            String directory = tokens[1].contains("*") ? "." : tokens[1];
+            lsDirectory(directory, pattern);
+        }
+    }
 
+    public static void lsDirectory(String dir, String pattern) {
+        Path path = Paths.get(dir);
+        try {
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(path, pattern)) {
+                for (Path entry : stream) {
+                    System.out.println(entry.getFileName());
+                }
+            }
+        }
+        catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
+
+
+
+//list of things to handle
+//ls
+//ls path
+//ls path1 path2 path3....
+//ls filename
+//ls ../  ...relative path
+//ls *extension  ....wild card
+//ls path/*extension
